52a51c53fd1a2192f0fb4e57fbc90a48a4e18f7d
yghysdr <yghysdr@163.com>
2020-08-12 19:43:27 +0800

    binner通讯-手动实现AIDL

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 ## AIDL基础
 参考 [android跨进程通信（IPC）：使用AIDL](https://blog.csdn.net/singwhatiwanna/article/details/17041691)
 
-### server
-远程服务端
+### server/client
+> 通过AIDL自动实现Binder实现跨进程通讯
+- server 远程服务端
+- client 本地客户端（与server不是同一个app）
+  - 自定数据类型的传输时候，需要将服务端的数据模型原样拷贝（包含包名）
+  - 通讯的时候，可以选择直接拷贝服务端的aidl文件，也可以拷贝aidl文件生成的java文件
 
-### client
-本地客户端
-
-- 自定数据类型的传输时候，需要将服务端的数据模型原样拷贝（包含包名）
-- 通讯的时候，可以选择直接拷贝服务端的aidl文件，也可以拷贝aidl文件生成的java文件
-
+### app
+手动实现Binder跨进城通讯，其中远程和本地都在一个app内
--- client/src/main/java/com/github/yghysdr/server/aidl/IStoreAidl.java
+++ client/src/main/java/com/github/yghysdr/server/aidl/IStoreAidl.java
@@ -3,132 +3,134 @@
  * Original file: /Users/yghysdr/androidLibrary/BinderApplication/server/src/main/aidl/com/github/yghysdr/server/aidl/IStoreAidl.aidl
  */
 package com.github.yghysdr.server.aidl;
-public interface IStoreAidl extends android.os.IInterface
-{
-/** Local-side IPC implementation stub class. */
-public static abstract class Stub extends android.os.Binder implements IStoreAidl
-{
-private static final String DESCRIPTOR = "com.github.yghysdr.server.aidl.IStoreAidl";
-/** Construct the stub at attach it to the interface. */
-public Stub()
-{
-this.attachInterface(this, DESCRIPTOR);
-}
-/**
- * Cast an IBinder object into an com.github.yghysdr.server.aidl.IStoreAidl interface,
- * generating a proxy if needed.
- */
-public static IStoreAidl asInterface(android.os.IBinder obj)
-{
-if ((obj==null)) {
-return null;
-}
-android.os.IInterface iin = obj.queryLocalInterface(DESCRIPTOR);
-if (((iin!=null)&&(iin instanceof IStoreAidl))) {
-return ((IStoreAidl)iin);
-}
-return new Proxy(obj);
-}
-@Override public android.os.IBinder asBinder()
-{
-return this;
-}
-@Override public boolean onTransact(int code, android.os.Parcel data, android.os.Parcel reply, int flags) throws android.os.RemoteException
-{
-String descriptor = DESCRIPTOR;
-switch (code)
-{
-case INTERFACE_TRANSACTION:
-{
-reply.writeString(descriptor);
-return true;
-}
-case TRANSACTION_getApplePrice:
-{
-data.enforceInterface(descriptor);
-int _result = this.getApplePrice();
-reply.writeNoException();
-reply.writeInt(_result);
-return true;
-}
-case TRANSACTION_getApple:
-{
-data.enforceInterface(descriptor);
-com.github.yghysdr.server.aidl.Apple _result = this.getApple();
-reply.writeNoException();
-if ((_result!=null)) {
-reply.writeInt(1);
-_result.writeToParcel(reply, android.os.Parcelable.PARCELABLE_WRITE_RETURN_VALUE);
-}
-else {
-reply.writeInt(0);
-}
-return true;
-}
-default:
-{
-return super.onTransact(code, data, reply, flags);
-}
-}
-}
-private static class Proxy implements IStoreAidl
-{
-private android.os.IBinder mRemote;
-Proxy(android.os.IBinder remote)
-{
-mRemote = remote;
-}
-@Override public android.os.IBinder asBinder()
-{
-return mRemote;
-}
-public String getInterfaceDescriptor()
-{
-return DESCRIPTOR;
-}
-@Override public int getApplePrice() throws android.os.RemoteException
-{
-android.os.Parcel _data = android.os.Parcel.obtain();
-android.os.Parcel _reply = android.os.Parcel.obtain();
-int _result;
-try {
-_data.writeInterfaceToken(DESCRIPTOR);
-mRemote.transact(Stub.TRANSACTION_getApplePrice, _data, _reply, 0);
-_reply.readException();
-_result = _reply.readInt();
-}
-finally {
-_reply.recycle();
-_data.recycle();
-}
-return _result;
-}
-@Override public com.github.yghysdr.server.aidl.Apple getApple() throws android.os.RemoteException
-{
-android.os.Parcel _data = android.os.Parcel.obtain();
-android.os.Parcel _reply = android.os.Parcel.obtain();
-com.github.yghysdr.server.aidl.Apple _result;
-try {
-_data.writeInterfaceToken(DESCRIPTOR);
-mRemote.transact(Stub.TRANSACTION_getApple, _data, _reply, 0);
-_reply.readException();
-if ((0!=_reply.readInt())) {
-_result = com.github.yghysdr.server.aidl.Apple.CREATOR.createFromParcel(_reply);
-}
-else {
-_result = null;
-}
-}
-finally {
-_reply.recycle();
-_data.recycle();
-}
-return _result;
-}
-}
-static final int TRANSACTION_getApplePrice = (android.os.IBinder.FIRST_CALL_TRANSACTION + 0);
-static final int TRANSACTION_getApple = (android.os.IBinder.FIRST_CALL_TRANSACTION + 1);
-}
-public int getApplePrice() throws android.os.RemoteException;
-public com.github.yghysdr.server.aidl.Apple getApple() throws android.os.RemoteException;
+
+public interface IStoreAidl extends android.os.IInterface {
+    /**
+     * Local-side IPC implementation stub class.
+     */
+    public static abstract class Stub extends android.os.Binder implements IStoreAidl {
+        private static final String DESCRIPTOR = "com.github.yghysdr.server.aidl.IStoreAidl";
+
+        /**
+         * Construct the stub at attach it to the interface.
+         */
+        public Stub() {
+            this.attachInterface(this, DESCRIPTOR);
+        }
+
+        /**
+         * Cast an IBinder object into an com.github.yghysdr.server.aidl.IStoreAidl interface,
+         * generating a proxy if needed.
+         */
+        public static IStoreAidl asInterface(android.os.IBinder obj) {
+            if ((obj == null)) {
+                return null;
+            }
+            android.os.IInterface iin = obj.queryLocalInterface(DESCRIPTOR);
+            if (((iin != null) && (iin instanceof IStoreAidl))) {
+                return ((IStoreAidl) iin);
+            }
+            return new Proxy(obj);
+        }
+
+        @Override
+        public android.os.IBinder asBinder() {
+            return this;
+        }
+
+        @Override
+        public boolean onTransact(int code, android.os.Parcel data, android.os.Parcel reply, int flags) throws android.os.RemoteException {
+            String descriptor = DESCRIPTOR;
+            switch (code) {
+                case INTERFACE_TRANSACTION: {
+                    reply.writeString(descriptor);
+                    return true;
+                }
+                case TRANSACTION_getApplePrice: {
+                    data.enforceInterface(descriptor);
+                    int _result = this.getApplePrice();
+                    reply.writeNoException();
+                    reply.writeInt(_result);
+                    return true;
+                }
+                case TRANSACTION_getApple: {
+                    data.enforceInterface(descriptor);
+                    com.github.yghysdr.server.aidl.Apple _result = this.getApple();
+                    reply.writeNoException();
+                    if ((_result != null)) {
+                        reply.writeInt(1);
+                        _result.writeToParcel(reply, android.os.Parcelable.PARCELABLE_WRITE_RETURN_VALUE);
+                    } else {
+                        reply.writeInt(0);
+                    }
+                    return true;
+                }
+                default: {
+                    return super.onTransact(code, data, reply, flags);
+                }
+            }
+        }
+
+        private static class Proxy implements IStoreAidl {
+            private android.os.IBinder mRemote;
+
+            Proxy(android.os.IBinder remote) {
+                mRemote = remote;
+            }
+
+            @Override
+            public android.os.IBinder asBinder() {
+                return mRemote;
+            }
+
+            public String getInterfaceDescriptor() {
+                return DESCRIPTOR;
+            }
+
+            @Override
+            public int getApplePrice() throws android.os.RemoteException {
+                android.os.Parcel _data = android.os.Parcel.obtain();
+                android.os.Parcel _reply = android.os.Parcel.obtain();
+                int _result;
+                try {
+                    _data.writeInterfaceToken(DESCRIPTOR);
+                    mRemote.transact(Stub.TRANSACTION_getApplePrice, _data, _reply, 0);
+                    _reply.readException();
+                    _result = _reply.readInt();
+                } finally {
+                    _reply.recycle();
+                    _data.recycle();
+                }
+                return _result;
+            }
+
+            @Override
+            public com.github.yghysdr.server.aidl.Apple getApple() throws android.os.RemoteException {
+                android.os.Parcel _data = android.os.Parcel.obtain();
+                android.os.Parcel _reply = android.os.Parcel.obtain();
+                com.github.yghysdr.server.aidl.Apple _result;
+                try {
+                    _data.writeInterfaceToken(DESCRIPTOR);
+                    mRemote.transact(Stub.TRANSACTION_getApple, _data, _reply, 0);
+                    _reply.readException();
+                    if ((0 != _reply.readInt())) {
+                        _result = com.github.yghysdr.server.aidl.Apple.CREATOR.createFromParcel(_reply);
+                    } else {
+                        _result = null;
+                    }
+                } finally {
+                    _reply.recycle();
+                    _data.recycle();
+                }
+                return _result;
+            }
+        }
+
+        static final int TRANSACTION_getApplePrice = (android.os.IBinder.FIRST_CALL_TRANSACTION + 0);
+        static final int TRANSACTION_getApple = (android.os.IBinder.FIRST_CALL_TRANSACTION + 1);
+    }
+
+    public int getApplePrice() throws android.os.RemoteException;
+
+    public com.github.yghysdr.server.aidl.Apple getApple() throws android.os.RemoteException;
 }
